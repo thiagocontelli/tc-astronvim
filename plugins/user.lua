95fb02a6fdb137988d468f85a5b458dfc08871e4
thiagocontelli <thiagocontellid@gmail.com>
2023-08-29 09:48:49 -0300

    change theme color scheme

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,9 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "catppuccin/nvim",
+    as = "catppuccin",
+    config = function() require("catppuccin").setup {} end,
+  },
 }
