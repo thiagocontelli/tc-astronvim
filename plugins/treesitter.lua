3303d833765ae3c1ff3f6f8e0bf982675d11433c
Thiago Contelli <thiagocontellid@gmail.com>
2023-08-30 18:51:43 -0300

    add autotag

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
-    })
-  end,
+  dependencies = {
+    { "windwp/nvim-ts-autotag" },
+    { "nvim-treesitter/nvim-treesitter-context", opts = { enable = true } },
+  },
+  opts = {
+    autotag = { enable = true },
+    ensure_installed = { "typescript", "tsx" },
+  },
 }
